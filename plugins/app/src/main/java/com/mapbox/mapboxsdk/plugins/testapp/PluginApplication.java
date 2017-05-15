623ce00e24c73ff310586161674ea3adcda12e74
Tobrun <tobrun@mapbox.com>
2017-05-15 17:13:12 +0200

    add leak canary to test application (#35)

--- plugins/app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/PluginApplication.java
+++ plugins/app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/PluginApplication.java
@@ -3,12 +3,19 @@ package com.mapbox.mapboxsdk.plugins.testapp;
 import android.app.Application;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.squareup.leakcanary.LeakCanary;
 
 public class PluginApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
     Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
   }
 }
