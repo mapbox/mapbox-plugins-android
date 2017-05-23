983fad37fd666f611f2471f171a18ea80c9b6363
Behrouz Khezry <bkhezry@gmail.com>
2017-05-23 15:34:42 +0430

    add geojson plugin

---
+++ plugins/geojson/src/main/java/com/mapbox/mapboxsdk/plugins/geojson/model/PolyData.java
@@ -0,0 +1,26 @@
+package com.mapbox.mapboxsdk.plugins.geojson.model;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+import java.util.List;
+
+public class PolyData {
+  private List<LatLng> points;
+  private String type;
+
+  public List<LatLng> getPoints() {
+    return points;
+  }
+
+  public void setPoints(List<LatLng> points) {
+    this.points = points;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
