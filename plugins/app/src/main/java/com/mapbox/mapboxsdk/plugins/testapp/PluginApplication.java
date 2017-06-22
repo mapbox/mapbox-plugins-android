1b580acc098660525ca8498ae7afee2b5f72b962
Pablo Guardiola <guardiola31337@gmail.com>
2017-06-22 17:41:03 +0200

    plant a debug tree timber instance (#50)

--- plugins/app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/PluginApplication.java
+++ plugins/app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/PluginApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.squareup.leakcanary.LeakCanary;
 
+import timber.log.Timber;
+
 public class PluginApplication extends Application {
 
   @Override
@@ -16,6 +18,13 @@ public class PluginApplication extends Application {
       return;
     }
     LeakCanary.install(this);
+    initializeLogger();
     Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+  }
+
+  private void initializeLogger() {
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    }
   }
 }
