52292841a4746bb955d37a5b76fe01b9f4fb5a56
Langston Smith <langston.smith@mapbox.com>
2017-08-08 15:13:53 -0700

    initial commit for tests

---
+++ plugins/localization/src/test/java/com/example/localization/LocalizationPluginTest.java
@@ -0,0 +1,28 @@
+package com.example.localization;
+
+
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class LocalizationPluginTest {
+  @Mock
+  MapboxMap mapboxMap;
+
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @Test(expected = NullPointerException.class)
+  public void testNonNullAnnotatedArgs() {
+    new LocalizationPlugin(null);
+  }
+
+  @Test
+  public void testSanity() {
+    new LocalizationPlugin(mapboxMap);
+  }
+}
