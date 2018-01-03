8cc936af0d0ba37aa2e0f95e1f6c2afd2b51cdbf
Cameron Mace <cameron@mapbox.com>
2018-01-03 10:06:46 -0500

    Change package name

---
+++ plugin-building/src/main/java/com/mapbox/mapboxsdk/plugins/building/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains logic used for the Mapbox Buildings Plugin.
+ *
+ * @since 0.1.0
+ */
+package com.mapbox.mapboxsdk.plugins.building;
---
+++ plugin-traffic/src/main/java/com/mapbox/mapboxsdk/plugins/traffic/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains logic used for the Mapbox Traffic Plugin.
+ *
+ * @since 0.1.0
+ */
+package com.mapbox.mapboxsdk.plugins.traffic;
