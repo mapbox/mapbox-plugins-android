d9e788f0c2277e013ef330af5fa16bda073c705b
Cameron Mace <cameron@mapbox.com>
2018-02-12 10:02:44 -0500

    Print out selected pplace in testapp

--- app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/places/PickerLauncherActivity.java
+++ app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/places/PickerLauncherActivity.java
@@ -1,10 +1,13 @@
 package com.mapbox.mapboxsdk.plugins.testapp.activity.places;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.mapbox.geocoding.v5.models.CarmenFeature;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.plugins.testapp.R;
 import com.mapbox.plugins.places.picker.PlacePicker;
@@ -26,5 +29,24 @@ public class PickerLauncherActivity extends AppCompatActivity {
   @OnClick(R.id.launch_location_picker)
   public void onClick(FloatingActionButton view) {
     startActivityForResult(new PlacePicker.IntentBuilder().accessToken(Mapbox.getAccessToken()).build(this), REQUEST_CODE);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        CarmenFeature carmenFeature = PlacePicker.getPlace(data);
+        String toastMsg = String.format("Place: %s", carmenFeature.placeName());
+        Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+      }
+    }
+
+
+
+
+
+
+
   }
 }
