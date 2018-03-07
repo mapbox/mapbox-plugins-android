c6f80ba8cf80c709d676b9631e709d2679df34b2
Cameron Mace <cameron@mapbox.com>
2018-03-07 16:33:35 -0500

    reverted version

--- release/javadoc.py
+++ release/javadoc.py
@@ -6,22 +6,22 @@ import subprocess
 # Generate Javadoc
 def generate_javadoc(version_name, plugin):
     print 'Version being used for Javadoc: %s' % version_name
-    update_current_version('../plugin-%s/gradle.properties' % plugin,
-                           constants.GRADLE_TOKEN, version_name)
-    subprocess.Popen(['make', 'javadoc-%s' % plugin], constants.PLUGIN_ROOT_PATH).wait()
+    update_current_version('../plugin-%s/gradle.properties' % plugin, constants.GRADLE_TOKEN,
+                           version_name)
+    subprocess.Popen(['make', 'javadoc-%s' % plugin], cwd=constants.PLUGIN_ROOT_PATH).wait()
     subprocess.Popen(['mv', 'release', version_name],
-                     '../plugin-%s/build/docs/javadoc/' % plugin).wait()
-    subprocess.Popen(['git', 'checkout', 'mb-pages'], constants.ANDROID_DOCS_ROOT_PATH).wait()
-    BRANCH_NAME = version_name + '-javadoc'
+                     cwd='../plugin-%s/build/docs/javadoc/' % plugin).wait()
+    subprocess.Popen(['git', 'checkout', 'mb-pages'], cwd=constants.ANDROID_DOCS_ROOT_PATH).wait()
+    BRANCH_NAME = plugin + '-' + version_name + '-javadoc'
     print 'Creating android-docs branch: %s' % BRANCH_NAME
-    subprocess.Popen(['git', 'checkout', '-b', BRANCH_NAME], '../../android-docs').wait()
+    subprocess.Popen(['git', 'checkout', '-b', BRANCH_NAME], cwd='../../android-docs').wait()
     subprocess.Popen(['mv', version_name, '../../../../../android-docs/api/plugins/%s' % plugin],
-                     '../plugin-%s/build/docs/javadoc/' % plugin).wait()
-    subprocess.Popen(['git', 'add', 'api/plugins'], constants.ANDROID_DOCS_ROOT_PATH).wait()
+                     cwd='../plugin-%s/build/docs/javadoc/' % plugin).wait()
+    subprocess.Popen(['git', 'add', 'api/plugins'], cwd=constants.ANDROID_DOCS_ROOT_PATH).wait()
     COMMIT_MESSAGE = version_name + "-javadoc-added"
     print 'Committing with message: %s' % COMMIT_MESSAGE
     subprocess.Popen(['git', 'commit', '-m', COMMIT_MESSAGE],
-                     constants.ANDROID_DOCS_ROOT_PATH).wait()
+                     cwd=constants.ANDROID_DOCS_ROOT_PATH).wait()
     subprocess.Popen(['git', 'push', '-u', 'origin', BRANCH_NAME],
-                     constants.ANDROID_DOCS_ROOT_PATH).wait()
+                     cwd=constants.ANDROID_DOCS_ROOT_PATH).wait()
     print 'Commit pushed, open a PR now'
