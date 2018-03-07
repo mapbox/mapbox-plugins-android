1b0e98de9be5f81bbbfcd7ccf4b49f687a5ccf7f
danesfeder <dan.nesfeder@mapbox.com>
2018-03-07 13:00:14 -0500

    Add missing long click listener

--- plugin-locationlayer/src/main/java/com/mapbox/mapboxsdk/plugins/locationlayer/LocationLayerPlugin.java
+++ plugin-locationlayer/src/main/java/com/mapbox/mapboxsdk/plugins/locationlayer/LocationLayerPlugin.java
@@ -495,6 +495,7 @@ public final class LocationLayerPlugin implements LifecycleObserver {
 
     mapView.addOnMapChangedListener(onMapChangedListener);
     mapboxMap.addOnMapClickListener(onMapClickListener);
+    mapboxMap.addOnMapLongClickListener(onMapLongClickListener);
     updateMapWithOptions(options);
 
     locationLayer = new LocationLayer(mapView, mapboxMap, options);
