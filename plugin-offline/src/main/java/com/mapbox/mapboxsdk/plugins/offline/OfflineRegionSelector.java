a1429c056b977073811e89ffdb2f73f1cbc1a51e
Cameron Mace <cameron@mapbox.com>
2018-04-27 13:01:41 -0400

    Added javadoc to offline classes

--- plugin-offline/src/main/java/com/mapbox/mapboxsdk/plugins/offline/OfflineRegionSelector.java
+++ plugin-offline/src/main/java/com/mapbox/mapboxsdk/plugins/offline/OfflineRegionSelector.java
@@ -1,7 +1,9 @@
 package com.mapbox.mapboxsdk.plugins.offline;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 
 import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
 import com.mapbox.mapboxsdk.plugins.offline.model.NotificationOptions;
@@ -11,12 +13,50 @@ import com.mapbox.mapboxsdk.plugins.offline.ui.OfflineActivity;
 import static com.mapbox.mapboxsdk.plugins.offline.offline.OfflineConstants.RETURNING_DEFINITION;
 import static com.mapbox.mapboxsdk.plugins.offline.offline.OfflineConstants.RETURNING_REGION_NAME;
 
+/**
+ * While the offline plugin includes a service for optimally launching an offline download session,
+ * the plugin also includes UI components which also assist in providing a way for your app users to
+ * select the region in which they'd like to download the region they desire.
+ * <p>
+ * This specific class is used to build an intent which launches the {@link OfflineActivity} which
+ * provides your users with a way to define the region which they'd like to download. This includes
+ * automatically providing the region definition with a name; Whether that's the default name or the
+ * name of the region the map's camera position is currently located.
+ * </p><p>
+ * The provided Intent builder in this class should be built with the options you would like to
+ * provide, then passed into the {@link Activity#startActivityForResult(Intent, int)} or related
+ * methods. Inside the same activity being used to launch this activity you'll need to
+ * {@code @Override} {@link Activity#onActivityResult(int, int, Intent)} and use the provided static
+ * methods also provided in this class.
+ * </p><p>
+ * Note that if you are using this exclusively inside your app with a Mapbox Map not already being
+ * used somewhere else, you'll need to provide a Mapbox access token by overriding your applications
+ * {@code onCreate} method and placing {@link com.mapbox.mapboxsdk.Mapbox#getInstance(Context, String)}
+ * inside the override method.
+ * </p>
+ *
+ * @since 0.1.0
+ */
 public class OfflineRegionSelector {
 
   private OfflineRegionSelector() {
     // No Instances
   }
 
+  /**
+   * Use this method to take the returning {@link Intent} data and construct a {@link OfflineDownloadOptions}
+   * instance which can be used for starting a new offline region download.
+   *
+   * @param data     the {@link Activity#startActivityForResult(Intent, int)} which this method should
+   *                 be used in provides the returning intent which should be provided in this param
+   * @param metadata Add additional metadata to the {@link OfflineTilePyramidRegionDefinition}, note
+   *                 to make sure not to override the region definition name if you still wish to use
+   *                 it
+   * @return a new {@link OfflineDownloadOptions} instance which can be used to launch the download
+   * service using
+   * {@link com.mapbox.mapboxsdk.plugins.offline.offline.OfflinePlugin#startDownload(OfflineDownloadOptions)}
+   * @since 0.1.0
+   */
   public static OfflineDownloadOptions getOfflineDownloadOptions(final Intent data, byte[] metadata) {
     return OfflineDownloadOptions.builder()
       .definition(getRegionDefinition(data))
@@ -25,6 +65,20 @@ public class OfflineRegionSelector {
       .build();
   }
 
+  /**
+   * Use this method to take the returning {@link Intent} data and construct a {@link OfflineDownloadOptions}
+   * instance which can be used for starting a new offline region download.
+   *
+   * @param data                the {@link Activity#startActivityForResult(Intent, int)} which this
+   *                            method should be used in provides the returning intent which should
+   *                            be provided in this param
+   * @param notificationOptions the {@link NotificationOptions} object you've constructed to be used
+   *                            when launching the offline region download service.
+   * @return a new {@link OfflineDownloadOptions} instance which can be used to launch the download
+   * service using
+   * {@link com.mapbox.mapboxsdk.plugins.offline.offline.OfflinePlugin#startDownload(OfflineDownloadOptions)}
+   * @since 0.1.0
+   */
   public static OfflineDownloadOptions getOfflineDownloadOptions(final Intent data,
                                                                  NotificationOptions notificationOptions) {
     return OfflineDownloadOptions.builder()
@@ -34,8 +88,26 @@ public class OfflineRegionSelector {
       .build();
   }
 
+  /**
+   * Use this method to take the returning {@link Intent} data and construct a {@link OfflineDownloadOptions}
+   * instance which can be used for starting a new offline region download.
+   *
+   * @param data                the {@link Activity#startActivityForResult(Intent, int)} which this
+   *                            method should be used in provides the returning intent which should
+   *                            be provided in this param
+   * @param notificationOptions the {@link NotificationOptions} object you've constructed to be used
+   *                            when launching the offline region download service.
+   * @param metadata            Add additional metadata to the {@link OfflineTilePyramidRegionDefinition},
+   *                            note to make sure not to override the region definition name if you
+   *                            still wish to use it
+   * @return a new {@link OfflineDownloadOptions} instance which can be used to launch the download
+   * service using
+   * {@link com.mapbox.mapboxsdk.plugins.offline.offline.OfflinePlugin#startDownload(OfflineDownloadOptions)}
+   * @since 0.1.0
+   */
   public static OfflineDownloadOptions getOfflineDownloadOptions(final Intent data,
-                                                   NotificationOptions notificationOptions, byte[] metadata) {
+                                                                 NotificationOptions notificationOptions,
+                                                                 byte[] metadata) {
     return OfflineDownloadOptions.builder()
       .definition(getRegionDefinition(data))
       .regionName(getRegionName(data))
@@ -44,22 +116,62 @@ public class OfflineRegionSelector {
       .build();
   }
 
+  /**
+   * Returns the {@link OfflineTilePyramidRegionDefinition} which was created when the user was
+   * inside the {@link OfflineActivity}.
+   *
+   * @param data the {@link Activity#startActivityForResult(Intent, int)} which this method should
+   *             be used in provides the returning intent which should be provided in this param
+   * @return the {@link OfflineTilePyramidRegionDefinition} which was created inside the
+   * {@link OfflineActivity}
+   * @since 0.1.0
+   */
   public static OfflineTilePyramidRegionDefinition getRegionDefinition(Intent data) {
     return data.getParcelableExtra(RETURNING_DEFINITION);
   }
 
-  public static String getRegionName(Intent data) {
+  /**
+   * The {@link OfflineActivity} class will try to provide a region name which is either the default
+   * region naming string or, depending on the map's camera position and where it is positioned over
+   * the map.
+   *
+   * @param data the {@link Activity#startActivityForResult(Intent, int)} which this method should
+   *             be used in provides the returning intent which should be provided in this param
+   * @return either a string containing the default region name or the actual region name which the
+   * map's camera position was last placed over
+   * @since 0.1.0
+   */
+  public static String getRegionName(@NonNull Intent data) {
     return data.getStringExtra(RETURNING_REGION_NAME);
   }
 
+  /**
+   * Useful for building an {@link Intent} which can be used to launch the {@link OfflineActivity}
+   * allowing your app user to select a region which they'd like to download.
+   *
+   * @since 0.1.0
+   */
   public static class IntentBuilder {
 
     Intent intent;
 
+    /**
+     * Construct a new instance of this builder.
+     *
+     * @since 0.1.0
+     */
     public IntentBuilder() {
       intent = new Intent();
     }
 
+    /**
+     * Build a new {@link Intent} object which should be used to launch the {@link OfflineActivity}.
+     *
+     * @param activity pass in the current activity which you intend to use
+     *                 {@link Activity#startActivityForResult(Intent, int)} in.
+     * @return a new {@link Intent} which should be used to launch the {@link OfflineActivity}
+     * @since 0.1.0
+     */
     public Intent build(Activity activity) {
       intent.setClass(activity, OfflineActivity.class);
       return intent;
--- plugin-offline/src/main/java/com/mapbox/mapboxsdk/plugins/offline/model/OfflineDownloadOptions.java
+++ plugin-offline/src/main/java/com/mapbox/mapboxsdk/plugins/offline/model/OfflineDownloadOptions.java
@@ -8,6 +8,7 @@ import com.google.auto.value.AutoValue;
 import com.mapbox.mapboxsdk.offline.OfflineRegion;
 import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
 import com.mapbox.mapboxsdk.plugins.offline.offline.OfflineDownloadService;
+import com.mapbox.mapboxsdk.plugins.offline.offline.OfflinePlugin;
 
 import java.util.UUID;
 
@@ -21,24 +22,76 @@ import java.util.UUID;
 @AutoValue
 public abstract class OfflineDownloadOptions implements Parcelable {
 
+  /**
+   * The download definition which is used to actually launch the download inside the service. This
+   * object particularly contains information such as the region bounding box, zoom levels, pixel
+   * density, etc.
+   *
+   * @return the Map SDK Offline region definition which can be used to acquire region download
+   * definition information
+   * @since 0.1.0
+   */
   @NonNull
   public abstract OfflineTilePyramidRegionDefinition definition();
 
+  /**
+   * Launching the service requires a notification, using the {@link NotificationOptions} class
+   * allows you to customize this notification and will be displayed for the offline download region
+   * while the service is running in the background.
+   *
+   * @return the {@link NotificationOptions} which will be used during the download process
+   * @since 0.1.0
+   */
   public abstract NotificationOptions notificationOptions();
 
+  /**
+   * A convenient method for providing the offline region a name which can then be used later to
+   * identify the specific region in the database of offline region tiles.
+   *
+   * @return the string which represents this offline regions name
+   * @since 0.1.0
+   */
   @Nullable
   public abstract String regionName();
 
+  /**
+   * Metadata can optionally be added to your offline download region which can contain any
+   * additional information you desire.
+   *
+   * @return a byte array representing the optional metadata
+   * @since 0.1.0
+   */
   @SuppressWarnings("mutable")
   public abstract byte[] metadata();
 
   public abstract long regionId();
 
+  /**
+   * Current download progress for this specific offline region. This will be 0 until the download
+   * actually starts and will be a value between 0 and 100 once the download begins.
+   * {@link OfflinePlugin#onProgressChanged(OfflineDownloadOptions, int)} should be used during the
+   * download session to get progress updates.
+   *
+   * @return an integer value between 0 and 100 representing the download progress
+   * @since 0.1.0
+   */
   public abstract int progress();
 
+  /**
+   * A region id which the service provides so internally identifying a particular region is easier
+   * during the download session. You'll rarely, if ever, need to use this information.
+   *
+   * @return a long, unique, identifier for this offline download region
+   * @since 0.1.0
+   */
   @NonNull
   public abstract Long uuid();
 
+  /**
+   * Used to build a new instance of this class.
+   *
+   * @return this classes builder class
+   */
   public static Builder builder() {
     return new AutoValue_OfflineDownloadOptions.Builder()
       .uuid(UUID.randomUUID().getMostSignificantBits())
@@ -48,25 +101,103 @@ public abstract class OfflineDownloadOptions implements Parcelable {
     // TODO user must provide a notificationOptions object
   }
 
+  /**
+   * A convenient way to build a new instance of this class using all of the same values this current
+   * instance has. Use this when you wish to modify a single entry.
+   *
+   * @return a new {@link Builder} instance with all the same values this classes instance contains
+   * @since 0.1.0
+   */
   public abstract Builder toBuilder();
 
+  /**
+   * The Builder class in charge of constructing this class and setting the values accordingly.
+   *
+   * @since 0.1.0
+   */
   @AutoValue.Builder
   public abstract static class Builder {
 
+    /**
+     * <strong>Internal usage only</strong>
+     * <p>
+     * A region id which the service provides so internally identifying a particular region is easier
+     * during the download session.
+     * </p>
+     *
+     * @param uuid a long, unique, identifier for this offline download region provided by the
+     *             download service.
+     * @return this builder for chaining options together
+     * @since 0.1.0
+     */
     abstract Builder uuid(@NonNull Long uuid);
 
+    /**
+     * Current download progress for this specific offline region. This will be 0 until the download
+     * actually starts and will be a value between 0 and 100 once the download begins.
+     * {@link OfflinePlugin#onProgressChanged(OfflineDownloadOptions, int)} should be used during the
+     * download session to get progress updates.
+     *
+     * @param progress an integer value between 0 and 100 representing the download progress
+     * @return this builder for chaining options together
+     * @since 0.1.0
+     */
     public abstract Builder progress(int progress);
 
+    /**
+     * The download definition which is used to actually launch the download inside the service. This
+     * object particularly contains information such as the region bounding box, zoom levels, pixel
+     * density, etc.
+     *
+     * @param definition the Map SDK Offline region definition which can be used to acquire region
+     *                   download definition information
+     * @return this builder for chaining options together
+     * @since 0.1.0
+     */
     public abstract Builder definition(@NonNull OfflineTilePyramidRegionDefinition definition);
 
+    /**
+     * Launching the service requires a notification, using the {@link NotificationOptions} class
+     * allows you to customize this notification and will be displayed for the offline download region
+     * while the service is running in the background.
+     *
+     * @param notificationOptions the {@link NotificationOptions} which will be used during the
+     *                            download process
+     * @return this builder for chaining options together
+     * @since 0.1.0
+     */
     public abstract Builder notificationOptions(NotificationOptions notificationOptions);
 
+    /**
+     * A convenient method for providing the offline region a name which can then be used later to
+     * identify the specific region in the database of offline region tiles.
+     *
+     * @param regionName the string which represents this offline regions name
+     * @return this builder for chaining options together
+     * @since 0.1.0
+     */
     public abstract Builder regionName(@Nullable String regionName);
 
+    /**
+     * Metadata can optionally be added to your offline download region which can contain any
+     * additional information you desire.
+     *
+     * @param metadata a byte array representing the optional metadata
+     * @return this builder for chaining options together
+     * @since 0.1.0
+     */
     public abstract Builder metadata(@NonNull byte[] metadata);
 
+    /**
+     * Build a new {@link OfflineDownloadOptions} instance using the information and values provided
+     * inside this builder class.
     public abstract Builder regionId(long regionId);
 
+     *
+     * @return a new instance of the {@link OfflineDownloadOptions} which is using the values you
+     * provided in this builder
+     * @since 0.1.0
+     */
     public abstract OfflineDownloadOptions build();
   }
 }
