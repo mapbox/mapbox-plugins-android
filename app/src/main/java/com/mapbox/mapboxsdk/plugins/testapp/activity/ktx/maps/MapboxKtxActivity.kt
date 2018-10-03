4c3d0a2d8b0ac0e60e680c3476a5ff7592324aa0
Tobrun <tobrun.van.nuland@gmail.com>
2018-10-03 11:04:58 +0200

    [maps-ktx] - add initial project setup, query rendered features
    [maps-ktx] - point latlng convenience methods

---
+++ app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/ktx/maps/MapboxKtxActivity.kt
@@ -0,0 +1,71 @@
+package com.mapbox.mapboxsdk.plugins.testapp.activity.ktx.maps
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.plugins.maps.queryRenderedFeatures
+import com.mapbox.mapboxsdk.plugins.testapp.R
+import kotlinx.android.synthetic.main.activity_maps_ktx.*
+
+class MapboxKtxActivity : AppCompatActivity(), OnMapReadyCallback, MapboxMap.OnMapClickListener {
+
+    private var mapboxMap: MapboxMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps_ktx)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        this.mapboxMap = mapboxMap
+        mapboxMap?.addOnMapClickListener(this)
+    }
+
+    override fun onMapClick(point: LatLng) {
+        val features = mapboxMap?.queryRenderedFeatures(point)
+        features?.first().let {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapboxMap?.removeOnMapClickListener (this)
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+}
