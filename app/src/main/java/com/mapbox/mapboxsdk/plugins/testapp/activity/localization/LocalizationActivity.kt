eba19c76b5c27ea60af78b7a82cfe3723fa341b8
Łukasz Paczos <lukasz.paczos@mapbox.com>
2018-12-17 16:48:02 +0100

    [localization] Update localization plugin with the Maps SDK v7.0.0

--- app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/localization/LocalizationActivity.kt
+++ app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/localization/LocalizationActivity.kt
@@ -16,161 +16,161 @@ import kotlinx.android.synthetic.main.activity_localization.*
 
 class LocalizationActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private var localizationPlugin: LocalizationPlugin? = null
-    private var mapboxMap: MapboxMap? = null
-    private var mapIsLocalized: Boolean = false
+  private var localizationPlugin: LocalizationPlugin? = null
+  private var mapboxMap: MapboxMap? = null
+  private var mapIsLocalized: Boolean = false
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_localization)
-        mapIsLocalized = true
-        Toast.makeText(this, R.string.change_language_instruction, Toast.LENGTH_LONG).show()
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_localization)
+    mapIsLocalized = true
+    Toast.makeText(this, R.string.change_language_instruction, Toast.LENGTH_LONG).show()
+    mapView.onCreate(savedInstanceState)
+    mapView.getMapAsync(this)
 
-        fabLocalize.setOnClickListener{
-            mapIsLocalized = if (mapIsLocalized) {
-                localizationPlugin?.setMapLanguage(MapLocale(MapLocale.FRENCH))
-                Toast.makeText(this, R.string.map_not_localized, Toast.LENGTH_SHORT).show()
-                false
-            } else {
-                localizationPlugin?.matchMapLanguageWithDeviceDefault()
-                Toast.makeText(this, R.string.map_localized, Toast.LENGTH_SHORT).show()
-                true
-            }
-        }
-
-        fabCamera.setOnClickListener{
-            val locale = nextMapLocale
-            localizationPlugin?.setMapLanguage(locale)
-            localizationPlugin?.setCameraToLocaleCountry(locale, 25)
-        }
-
-        fabStyles.setOnClickListener{
-            mapboxMap?.setStyle(Style.Builder().fromUrl(Utils.nextStyle))
-        }
+    fabLocalize.setOnClickListener {
+      mapIsLocalized = if (mapIsLocalized) {
+        localizationPlugin?.setMapLanguage(MapLocale(MapLocale.FRENCH))
+        Toast.makeText(this, R.string.map_not_localized, Toast.LENGTH_SHORT).show()
+        false
+      } else {
+        localizationPlugin?.matchMapLanguageWithDeviceDefault()
+        Toast.makeText(this, R.string.map_localized, Toast.LENGTH_SHORT).show()
+        true
+      }
     }
 
-    override fun onMapReady(mapboxMap: MapboxMap) {
-        this.mapboxMap = mapboxMap
-        mapboxMap.setStyle(Style.MAPBOX_STREETS){
-            localizationPlugin = LocalizationPlugin(mapView, mapboxMap)
-            localizationPlugin?.matchMapLanguageWithDeviceDefault()
-        }
+    fabCamera.setOnClickListener {
+      val locale = nextMapLocale
+      localizationPlugin?.setMapLanguage(locale)
+      localizationPlugin?.setCameraToLocaleCountry(locale, 25)
     }
 
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
+    fabStyles.setOnClickListener {
+      mapboxMap?.setStyle(Style.Builder().fromUrl(Utils.nextStyle))
     }
+  }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
+  override fun onMapReady(mapboxMap: MapboxMap) {
+    this.mapboxMap = mapboxMap
+    mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+      localizationPlugin = LocalizationPlugin(mapView, mapboxMap, it)
+      localizationPlugin?.matchMapLanguageWithDeviceDefault()
     }
+  }
 
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
+  override fun onStart() {
+    super.onStart()
+    mapView.onStart()
+  }
 
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
+  override fun onResume() {
+    super.onResume()
+    mapView.onResume()
+  }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
+  override fun onPause() {
+    super.onPause()
+    mapView.onPause()
+  }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
+  override fun onStop() {
+    super.onStop()
+    mapView.onStop()
+  }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView.onSaveInstanceState(outState)
+  }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_languages, menu)
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu_languages, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      R.id.english -> {
+        localizationPlugin?.setMapLanguage(MapLocale.ENGLISH)
         return true
+      }
+      R.id.spanish -> {
+        localizationPlugin?.setMapLanguage(MapLocale.SPANISH)
+        return true
+      }
+      R.id.french -> {
+        localizationPlugin?.setMapLanguage(MapLocale.FRENCH)
+        return true
+      }
+      R.id.german -> {
+        localizationPlugin?.setMapLanguage(MapLocale.GERMAN)
+        return true
+      }
+      R.id.russian -> {
+        localizationPlugin?.setMapLanguage(MapLocale.RUSSIAN)
+        return true
+      }
+      R.id.chinese -> {
+        localizationPlugin?.setMapLanguage(MapLocale.CHINESE)
+        return true
+      }
+      R.id.simplified_chinese -> {
+        localizationPlugin?.setMapLanguage(MapLocale.SIMPLIFIED_CHINESE)
+        return true
+      }
+      R.id.portuguese -> {
+        localizationPlugin?.setMapLanguage(MapLocale.PORTUGUESE)
+        return true
+      }
+      R.id.arabic -> {
+        localizationPlugin?.setMapLanguage(MapLocale.ARABIC)
+        return true
+      }
+      R.id.japanese -> {
+        localizationPlugin?.setMapLanguage(MapLocale.JAPANESE)
+        return true
+      }
+      R.id.korean -> {
+        localizationPlugin?.setMapLanguage(MapLocale.KOREAN)
+        return true
+      }
+      R.id.local -> {
+        localizationPlugin?.setMapLanguage(MapLocale.LOCAL_NAME)
+        return true
+      }
+      android.R.id.home -> {
+        finish()
+        return true
+      }
     }
+    return super.onOptionsItemSelected(item)
+  }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.english -> {
-                localizationPlugin?.setMapLanguage(MapLocale.ENGLISH)
-                return true
-            }
-            R.id.spanish -> {
-                localizationPlugin?.setMapLanguage(MapLocale.SPANISH)
-                return true
-            }
-            R.id.french -> {
-                localizationPlugin?.setMapLanguage(MapLocale.FRENCH)
-                return true
-            }
-            R.id.german -> {
-                localizationPlugin?.setMapLanguage(MapLocale.GERMAN)
-                return true
-            }
-            R.id.russian -> {
-                localizationPlugin?.setMapLanguage(MapLocale.RUSSIAN)
-                return true
-            }
-            R.id.chinese -> {
-                localizationPlugin?.setMapLanguage(MapLocale.CHINESE)
-                return true
-            }
-            R.id.simplified_chinese -> {
-                localizationPlugin?.setMapLanguage(MapLocale.SIMPLIFIED_CHINESE)
-                return true
-            }
-            R.id.portuguese -> {
-                localizationPlugin?.setMapLanguage(MapLocale.PORTUGUESE)
-                return true
-            }
-            R.id.arabic -> {
-                localizationPlugin?.setMapLanguage(MapLocale.ARABIC)
-                return true
-            }
-            R.id.japanese -> {
-                localizationPlugin?.setMapLanguage(MapLocale.JAPANESE)
-                return true
-            }
-            R.id.korean -> {
-                localizationPlugin?.setMapLanguage(MapLocale.KOREAN)
-                return true
-            }
-            R.id.local -> {
-                localizationPlugin?.setMapLanguage(MapLocale.LOCAL_NAME)
-                return true
-            }
-            android.R.id.home -> {
-                finish()
-                return true
-            }
+  companion object {
+
+    private val LOCALES = arrayOf(MapLocale.CANADA, MapLocale.GERMANY, MapLocale.CHINA, MapLocale.US, MapLocale.CANADA_FRENCH, MapLocale.JAPAN, MapLocale.KOREA, MapLocale.FRANCE, MapLocale.SPAIN)
+
+    private var index: Int = 0
+
+    val nextMapLocale: MapLocale
+      get() {
+        index++
+        if (index == LOCALES.size) {
+          index = 0
         }
-        return super.onOptionsItemSelected(item)
-    }
-
-    companion object {
-
-        private val LOCALES = arrayOf(MapLocale.CANADA, MapLocale.GERMANY, MapLocale.CHINA, MapLocale.US, MapLocale.CANADA_FRENCH, MapLocale.JAPAN, MapLocale.KOREA, MapLocale.FRANCE, MapLocale.SPAIN)
-
-        private var index: Int = 0
-
-        val nextMapLocale: MapLocale
-            get() {
-                index++
-                if (index == LOCALES.size) {
-                    index = 0
-                }
-                return LOCALES[index]
-            }
-    }
+        return LOCALES[index]
+      }
+  }
 }
 
