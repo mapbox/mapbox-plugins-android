cd8831374cf263c123d365a3b025e636f28c4233
Łukasz Paczos <lukasz.paczos@mapbox.com>
2018-12-18 17:12:15 +0100

    [traffic] Update traffic plugin with the Maps SDK v7.0.0

--- plugin-traffic/src/test/java/com/mapbox/mapboxsdk/plugins/traffic/TrafficPluginTest.java
+++ plugin-traffic/src/test/java/com/mapbox/mapboxsdk/plugins/traffic/TrafficPluginTest.java
@@ -2,10 +2,12 @@ package com.mapbox.mapboxsdk.plugins.traffic;
 
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.Style;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -15,29 +17,40 @@ import static org.junit.Assert.assertTrue;
 public class TrafficPluginTest {
 
   @Mock
-  MapView mapView;
+  private MapView mapView;
 
   @Mock
-  MapboxMap mapboxMap;
+  private MapboxMap mapboxMap;
+
+  @Mock
+  private Style style;
 
   @Rule
   public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-  @Test(expected = NullPointerException.class)
+  @Test(expected = RuntimeException.class)
   public void testNonNullAnnotatedArgs() {
-    new TrafficPlugin(null, null);
+    new TrafficPlugin(null, null, null);
   }
 
   @Test
   public void testSanity() {
-    new TrafficPlugin(mapView, mapboxMap);
+    Mockito.when(style.isFullyLoaded()).thenReturn(true);
+    new TrafficPlugin(mapView, mapboxMap, style);
   }
 
   @Test
   public void testToggle() {
-    TrafficPlugin trafficPlugin = new TrafficPlugin(mapView, mapboxMap);
+    Mockito.when(style.isFullyLoaded()).thenReturn(true);
+    TrafficPlugin trafficPlugin = new TrafficPlugin(mapView, mapboxMap, style);
     assertFalse(trafficPlugin.isVisible());
     trafficPlugin.setVisibility(true);
     assertTrue(trafficPlugin.isVisible());
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testNotLoadedStyle() {
+    Mockito.when(style.isFullyLoaded()).thenReturn(false);
+    new TrafficPlugin(mapView, mapboxMap, style);
   }
 }
