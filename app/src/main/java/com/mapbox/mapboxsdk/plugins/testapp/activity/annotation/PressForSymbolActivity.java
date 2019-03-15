ff61e05423e14d000231d5b15bc252e8ee479d49
Sam Fader <sam.fader@mapbox.com>
2019-03-15 11:06:40 -0700

    testing out symbol click

--- app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/annotation/PressForSymbolActivity.java
+++ app/src/main/java/com/mapbox/mapboxsdk/plugins/testapp/activity/annotation/PressForSymbolActivity.java
@@ -10,6 +10,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
+
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -17,6 +19,9 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
+import com.mapbox.mapboxsdk.plugins.annotation.OnAnnotationClickListener;
+import com.mapbox.mapboxsdk.plugins.annotation.OnSymbolClickListener;
 import com.mapbox.mapboxsdk.plugins.testapp.R;
 import com.mapbox.mapboxsdk.plugins.testapp.Utils;
 
@@ -29,6 +34,8 @@ import com.mapbox.mapboxsdk.plugins.testapp.Utils;
 public class PressForSymbolActivity extends AppCompatActivity {
 
   public static final String ID_ICON = "id-icon";
+  private static final String MAKI_ICON_CAFE = "cafe-15";
+  private static final String MAKI_ICON_HARBOR = "harbor-15";
   private SymbolManager symbolManager;
   private MapView mapView;
   private MapboxMap mapboxMap;
@@ -43,24 +50,47 @@ public class PressForSymbolActivity extends AppCompatActivity {
     mapView.getMapAsync(map -> {
       mapboxMap = map;
       mapboxMap.setCameraPosition(new CameraPosition.Builder()
-        .target(new LatLng(60.169091, 24.939876))
-        .zoom(12)
-        .tilt(20)
-        .bearing(90)
-        .build()
+              .target(new LatLng(60.169091, 24.939876))
+              .zoom(12)
+              .tilt(20)
+              .bearing(90)
+              .build()
       );
-      mapboxMap.addOnMapLongClickListener(this::addSymbol);
-      mapboxMap.addOnMapClickListener(this::addSymbol);
+//      mapboxMap.addOnMapLongClickListener(this::addSymbol);
+//      mapboxMap.addOnMapClickListener(this::addSymbol);
       mapboxMap.setStyle(getStyleBuilder(Style.MAPBOX_STREETS), style -> {
         findViewById(R.id.fabStyles).setOnClickListener(v ->
-          mapboxMap.setStyle(getStyleBuilder(Utils.INSTANCE.getNextStyle())));
+                mapboxMap.setStyle(getStyleBuilder(Utils.INSTANCE.getNextStyle())));
 
         symbolManager = new SymbolManager(mapView, mapboxMap, style);
+        Symbol symbol = symbolManager.create(new SymbolOptions()
+                .withLatLng(new LatLng(60.169091, 24.939876))
+                .withIconImage(MAKI_ICON_HARBOR));
+        Log.v("Symbol", "symbol is " + symbol);
         symbolManager.setIconAllowOverlap(true);
         symbolManager.setTextAllowOverlap(true);
+        symbolManager.addClickListener(new OnSymbolClickListener() {
+          @Override
+          public void onAnnotationClick(Symbol symbol) {
+            symbol.setIconImage(MAKI_ICON_CAFE);
+            Log.v("Symbol", "symbol is " + symbol);
+          }
+        });
+
       });
     });
   }
+//
+//  private boolean changeSymbol(Symbol symbol) {
+//    if (symbol == null) {
+//      return false;
+//    }
+//
+//    Log.v("Symbol", "symbol is " + symbol);
+//
+//    symbol.setIconImage(MAKI_ICON_CAFE);
+//    return true;
+//  }
 
   private boolean addSymbol(LatLng point) {
     if (symbolManager == null) {
@@ -68,15 +98,15 @@ public class PressForSymbolActivity extends AppCompatActivity {
     }
 
     symbolManager.create(new SymbolOptions()
-      .withLatLng(point)
-      .withIconImage(ID_ICON)
+            .withLatLng(point)
+            .withIconImage(ID_ICON)
     );
     return true;
   }
 
   private Style.Builder getStyleBuilder(@NonNull String styleUrl) {
     return new Style.Builder().fromUrl(styleUrl)
-      .withImage(ID_ICON, generateBitmap(R.drawable.mapbox_ic_place));
+            .withImage(ID_ICON, generateBitmap(R.drawable.mapbox_ic_place));
   }
 
   @Override
@@ -139,7 +169,7 @@ public class PressForSymbolActivity extends AppCompatActivity {
     } else {
       // width and height are equal for all assets since they are ovals.
       Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
-        drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+              drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
       Canvas canvas = new Canvas(bitmap);
       drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
       drawable.draw(canvas);
@@ -147,4 +177,3 @@ public class PressForSymbolActivity extends AppCompatActivity {
     }
   }
 }
-
